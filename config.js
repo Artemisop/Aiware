31cad6cbad9567ded7731686fb59de9131ce26c1
Artemisop <grakon0303@gmail.com>
2026-09-03 11:41:06 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Вставь сюда данные из Supabase → Project Settings → API
+const SUPABASE_URL = "https://zeytgzsikenxxbqfqvoc.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_cfVu4AkiVmk5XBaCnlA8_Q_Os-4sKX_";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
